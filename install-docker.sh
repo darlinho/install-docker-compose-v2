1a4236701c512bc7b28692115b9af7b1303b769d
darlinho <56307301+darlinho@users.noreply.github.com>
2025-07-20 21:21:37 -0400

    Create install-docker.sh

---
+++ install-docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+# Mettez à jour la liste des paquets
+sudo apt-get update -y
+
+# Installez les prérequis
+sudo apt-get install -y ca-certificates curl
+
+# Ajoutez la clé GPG officielle de Docker
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Ajoutez le repository Docker
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y
+
+# Installez Docker et Docker Compose plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
